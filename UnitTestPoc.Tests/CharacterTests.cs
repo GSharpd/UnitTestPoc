800aaa68ad832fe159accf61c83efe8ae4f0756b
Gustavo Vieira <gmtnvieira@gmail.com>
2020-08-08 16:21:38 -0300

    initial commit

---
+++ UnitTestPoc.Tests/CharacterTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace UnitTestPoc.Tests
+{
+    [TestFixture]
+    public class CharacterTests
+    {
+        [Test]
+        public void TestHuman()
+        {
+            Character charOne = new Character(Race.Human);
+            charOne.Damage(8);
+            Assert.That(charOne.Health, Is.EqualTo(2));
+        }
+    }
+}
